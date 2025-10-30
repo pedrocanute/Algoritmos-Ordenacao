552c5086e5e4921c6a1b2bb4cdecaaa29367db6d
pedrocanute <pedro.canute2@gmail.com>
2025-10-30 15:02:35 -0300

    Adcionado insertion sort

---
+++ Insertion Sort/Insertion Sort.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+void insertion_sort(int array[], int n) {
+
+	int j, chave;
+
+	for (int i = 0; i < n; i++) {
+
+		chave = array[i];
+		j = i - 1;
+
+		while (j >= 0 && array[j] > chave) {
+			array[j + 1] = array[j];
+			j--;
+		}
+		array[j + 1] = chave;
+	}
+}
