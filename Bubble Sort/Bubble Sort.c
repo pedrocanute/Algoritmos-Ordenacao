7556466c51cf5e8c07bd9cea5be9a67acec6e268
pedrocanute <pedro.canute2@gmail.com>
2025-10-30 12:38:10 -0300

    Adicionado busca binaria e bubble sort

---
+++ Bubble Sort/Bubble Sort.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+void bubble_sort(int array[], int n) {
+
+	int temporario, trocou;
+
+	for (int i = 0; i < n - 1; i++) {
+		trocou = 0;
+		for (int j = 0; j < n - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				temporario = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temporario;
+				trocou = 1;
+			}
+		}
+		if (trocou == 0) break;
+	}
+}
+
+int main() {
+
+	int array[20];
+	srand(time(NULL));
+	printf("Array Original:\n");
+	for (int i = 0; i < 20; i++) {
+		array[i] = rand() % 21;
+		printf("%d ", array[i]);
+	}
+
+	bubble_sort(array, 20);
+	printf("\n\n");
+	printf("Array Ordenado:\n");
+
+	for (int i = 0; i < 20; i++) {
+		printf("%d ", array[i]);
+	}
+	return 0;
+}
---
+++ Busca Binaria/busca_binaria.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+//#include <time.h>
+
+
+int busca_binaria(int array[], int x, int left, int right) {
+
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+
+		if (array[mid] == x) {
+			return mid;
+		}
+		if (array[mid] < x)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return -1;
+}
+
+
+int main() {
+	
+	int array[100], left = 0, right = 99;
+	//srand(time(NULL));
+
+	for (int i = 0; i < 100; i++) {
+		array[i] = i;                      //rand() % 101;
+		printf("%d ", array[i]);
+	}
+
+	printf("\nIndice %d\n", busca_binaria(array, 32, left, right));
+
+
+	return 0;
+}
