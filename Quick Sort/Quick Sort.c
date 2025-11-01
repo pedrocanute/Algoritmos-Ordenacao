9700d6b6b38e524bc9fb60c3459cd2debbc503cc
pedrocanute <pedro.canute2@gmail.com>
2025-11-01 15:11:28 -0300

    Correcao para lomuto partition

--- Quick Sort/Quick Sort.c
+++ Quick Sort/Quick Sort.c
@@ -8,24 +8,24 @@ void troca(int a[], int i, int j) {
 
 }
 
-int partition(int a[], int e, int d) {
+int lomuto_partition(int a[], int e, int d) {
 
 	int pivo = a[d];
-	int i = e;
+	int i = e - 1;
 
-	for (int j = e; j < d - 1; j++) {
-		if (a[j] < pivo) {
-			troca(a, i, j);
+	for (int j = e; j < d ; j++) {
+		if (a[j] <= pivo) {
 			i++;
+			troca(a, i, j);
 		}
 	}
-	troca(a, i, d);
-	return i;
+	troca(a, i + 1, d);
+	return i + 1;
 }
 
 void quick_sort(int a[], int e, int d) {
 	if (e < d) {
-		int q = partition(a, e, d);
+		int q = lomuto_partition(a, e, d);
 		quick_sort(a, e, q - 1);
 		quick_sort(a, q + 1, d);
 
